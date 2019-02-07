5cfb7a368976605cbcf1085ca647fbe065dfb881
Montana Webb <montanafaith2000@hotmail.com>
2019-02-06 17:19:04 -0700

    "else ifs" + character moving
    else ifs practice and character moving left and right

---
+++ Assignments/Functions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+
+	public string weather;
+
+
+	void Start(){
+
+		Weather(weather);
+	}
+
+	void Weather(string weatherState){
+
+		if(weatherState == "Sunny"){
+			print("The sun is shining today!");
+		}
+		else if(weatherState == "Raining"){
+			print("It is soggy and wet today!");
+		}
+		else if(weatherState == "Windy"){
+			print("It is very windy today!");
+		}
+		else if(weatherState == "Foggy"){
+			print("Visability is very poor due to fog");
+		}
+		else if(weatherState == "Cloudy"){
+			print("The sky looks grey because the clouds are covering the sun.");
+		}
+		else if(weatherState == "Hurricane"){
+			print("It is rainy and windy and dangerous today.");
+		}
+		else if(weatherState == "Tornado"){
+			print("Do not go outside it is dangerous!");
+		}
+		else if(weatherState == "Hail"){
+			print("it is raining balls of ice today.");
+		}
+		else if(weatherState == "Snowy"){
+			print("It is very cold and snowy today!");
+		}
+		else if(weatherState == "Cats and dogs"){
+			print("It's actually raining cats and dogs right now..");
+		}
+		else{
+			print("I don't understand "+ weatherState);
+		}
+}
+}
