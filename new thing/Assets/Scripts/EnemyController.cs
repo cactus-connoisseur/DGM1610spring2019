63266215ffd8c2725eb01d8d129a53cc77c8c150
Montana Webb <montanafaith2000@hotmail.com>
2019-03-27 17:44:07 -0600

    Enemy controller
    enemy moves left and right withing walls

--- new thing/Assets/Scripts/EnemyController.cs
+++ new thing/Assets/Scripts/EnemyController.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+// movement variables
+public float moveSpeed;
+public bool moveRight;
+
+//wall check
+public Transform wallCheck;
+public float wallCheckRadius;
+public LayerMask whatIsWall;
+private bool hittingWall;
+
+//edge check
+private bool notAtEdge;
+public Transform edgeCheck;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+		hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+		if (hittingWall || !notAtEdge) {
+			moveRight = !moveRight;
+		}
+		if (moveRight) {
+			transform.localScale = new Vector3(-10f, 10f, 10f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+
+		else {
+			transform.localScale = new Vector3(10f, 10f, 10f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
 	}
 }
