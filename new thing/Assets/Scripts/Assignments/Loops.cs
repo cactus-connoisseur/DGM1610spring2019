90544ae1eb77bcb73baddfad8c192b9afe68b2b2
Montana Webb <montanafaith2000@hotmail.com>
2019-03-27 17:07:27 -0600

    kill code and level manager code
    code that will kill player and level manager for respawning

---
+++ new thing/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// for loop
+		//for(int i=100; 1 > bottles; i--){
+			//print(i +"bottles of whatever on the thing");
+		//}
+
+		//while(bottles <+ 100){
+			//print(bottles +"bottles of goo on the shoe");
+			//bottles ++;
+		//}
+
+
+		//do while loop
+		// bool shouldContinue = false; 
+
+		// do
+		// {
+		// 	print ("Hello world");
+
+		// }while(shouldContinue == true);
+
+
+		//For each Loop
+		string[] strings = new string[3];
+
+		strings[0] = "First string";
+		strings[1] = "Second string";
+		strings[2] = "Third  string";
+
+		foreach(string item in strings)
+		{
+			print (item);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
