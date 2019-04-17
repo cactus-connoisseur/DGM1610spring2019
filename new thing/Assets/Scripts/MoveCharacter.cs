0fa566d85ed7265db5b5194beb6a2166b070900b
Montana Webb <montanafaith2000@hotmail.com>
2019-04-17 17:05:54 -0600

    double jump, no slide, and more game art
    player wont slide and player gets one extra jump while in the air. new rock drawing.

--- new thing/Assets/Scripts/MoveCharacter.cs
+++ new thing/Assets/Scripts/MoveCharacter.cs
@@ -7,12 +7,16 @@ public class MoveCharacter : MonoBehaviour
 		// Player movement variables
         public float moveSpeed; 
         public float jumpHeight;
+		private bool doubleJump;
 
 		// Player grounded variables
 		private bool grounded;
 		public Transform groundCheck;
 		public float groundCheckRadius;
 		public LayerMask whatIsGround;
+
+		//Non-slide Player
+		private float moveVelocity;
 
 		public Animator animator;
 
@@ -21,8 +25,8 @@ public class MoveCharacter : MonoBehaviour
 					//Default to true on grounded
 
 					//animation reset 
-					animator.SetBool("isWalking",false);
-					animator.SetBool("isJumping",false);
+					// animator.SetBool("isWalking",false);
+					// animator.SetBool("isJumping",false);
 
 	    }
 
@@ -37,28 +41,37 @@ public class MoveCharacter : MonoBehaviour
 
 	 // Update is called once per frame
 	void Update () {
-		// Moves player left and right
+
+		//non slide player
+		moveVelocity = 0f;
+
+		// Moves player left and right with ket a and key d
 		 if(Input.GetKey(KeyCode.D)){
-			 GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//  GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed;
 			//  animator.SetBool("isWalking",true);
 		}
 			else if(Input.GetKeyUp (KeyCode.D)){
-				animator.SetBool("isWalking",false);
+			// animator.SetBool("isWalking",false);
 			}
 		 if(Input.GetKey(KeyCode.A)){
-			 GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			 //GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			 moveVelocity = -moveSpeed;
 			//  animator.SetBool("isWalking",true);
 		 }
 		 else if(Input.GetKeyUp (KeyCode.A)){
 			//  animator.SetBool("isWalking",false);
 		 }
 
-		// Makes player jump
+		 //tjis is outside of the conditionals so runs constantly
+		 GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+		// Makes player jump when w key is hit
         if(Input.GetKeyDown(KeyCode.W) && grounded){
             Jump();
         }
 
-		//player flip
+		//player flip when moving left to right
 		if (GetComponent<Rigidbody2D>().velocity.x > 0)
 			transform.localScale = new Vector3(0.1007464f,0.1007464f,0.1007464f);
 
@@ -66,16 +79,21 @@ public class MoveCharacter : MonoBehaviour
 			transform.localScale = new Vector3(-0.1007464f,0.1007464f,0.1007464f);
 
 			//double jump code
-			// if(grounded){
-			// 	//doubleJump = false;
-			// 	animator.SetBool("isJumping",false);
-			// }
+			if(grounded){
+				doubleJump = false;
+				// animator.SetBool("isJumping",false);
+			}
+
+			if(Input.GetKeyDown (KeyCode.W)&& !doubleJump && !grounded){
+				Jump();
+				doubleJump = true;
+			}
 
 
 	 }
 
 	 public void Jump(){
  		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
-		animator.SetBool("isJumping",true);
+		// animator.SetBool("isJumping",true);
 	 }
 }
